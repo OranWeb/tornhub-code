2d1d7054a813667e39f870903c41c74d79760817
OranWeb <46417116+OranWeb@users.noreply.github.com>
2020-08-22 16:47:11 +0530

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://demo.jamify.org`,
+    siteUrl: `https://torn.oran.pw`,
 
     // Enable infinite scroll (default: true)
     infiniteScroll: true,
@@ -10,10 +10,10 @@ module.exports = {
     postsPerPage: 3,
 
     // This allows an alternative site title for meta data for pages.
-    siteTitleMeta: `Gatsby Starter Ghost CMS`,
+    siteTitleMeta: `TornHub`,
 
     // This allows an site description for meta data for pages.
-    siteDescriptionMeta: `Turn your Ghost blog into a flaring fast static site with Gatsby`,
+    siteDescriptionMeta: `A Torn City Fan Site`,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
     shortTitle: `Jamify`,
